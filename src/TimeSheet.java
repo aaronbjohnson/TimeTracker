4160b1039889328a2afd56cb414817c99cc3a14e
Aaron Johnson <aj65461@gmail.com>
2020-12-24 11:41:54 -0500

    Change Project storage to use HashMaps and assign IDs

--- src/TimeSheet.java
+++ src/TimeSheet.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TimeSheet {
     private double grandTotal;
+    private static int id = 0;
+
+
+    private Map<Integer, Project> projectMap = new HashMap<Integer, Project>();
+
+    // todo: delete this I think
     private ArrayList<Project> projects;
 
     public TimeSheet(double grandTotal) {
@@ -9,17 +17,27 @@ public class TimeSheet {
         this.projects  = new ArrayList<Project>();
     }
 
-    public void addProject(Project newProject) {
-        projects.add(newProject);
+    public Project addProject(Project project) {
+        id++;
+        projectMap.put(id, project);
+        return project;
     }
 
+   /* public void addProject(Project newProject) {
+        projects.add(newProject);
+    }*/
+
     public int getNumberProjects() {
+        return projectMap.size();
+    }
+
+    /*public int getNumberProjects() {
         if (!(projects == null)) {
             return projects.size();
         } else {
             return 0;
         }
-    }
+    }*/
 
     public double getGrandTotal() {
         return grandTotal;
